645f83dd3dec202147a2d7f0f67b5f56b3fc9de4
Gadget_Vishwa <90371639+SKVhacks@users.noreply.github.com>
2024-07-12 23:28:30 +0530

    script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Front end developer","Fresher","Circuit Designer","Youtuber", "Drone Piolet", "PCB Designer"],
+        strings: ["Front end developer","Fresher","Circuit Designer","Ui/Ux Designer", "Drone Piolet", "PCB Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
@@ -48,7 +48,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Front end developer","Fresher","Circuit Designer","Youtuber", "Drone Piolet", "PCB Designer"],
+        strings: ["Front end developer","Fresher","Circuit Designer","Ui/Ux Designer", "Drone Piolet", "PCB Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
